95362fb426d3f9c129fd9c2c32d08cea61ecc93a
mmucahitballi <mmucahitballi@gmail.com>
2021-12-17 09:27:50 +0300

    duzenleme

--- Tables/dbo.Birds.sql
+++ Tables/dbo.Birds.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Birds]
 [BirdName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [TypeOfBird] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ScientificName] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Color] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Color] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[ColorSecond] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Birds] ADD CONSTRAINT [PK_Birds] PRIMARY KEY CLUSTERED ([ID]) WITH (ONLINE = ON) ON [PRIMARY]
